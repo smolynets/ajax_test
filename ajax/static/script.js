60952cf05de85285ccdbb3e562c21c489f580058
oleh Smolynets <smolynets@gmail.com>
2018-01-13 21:04:05 +0200

    version by Podoba(finish)

--- ajax/static/script.js
+++ ajax/static/script.js
@@ -1,46 +1,19 @@
-$(document).ready(function(){
-    var form = $('#form_buying_product');
-    console.log(form);
-
-
-    function basketUpdating(name, nmb){
-        var data = {};
-        data.name = name;
-        data.nmb = nmb;
-         var csrf_token = $('#form_buying_product [name="csrfmiddlewaretoken"]').val();
-         data["csrfmiddlewaretoken"] = csrf_token;
-
-         var url = form.attr("action");
-
-        console.log(data)
-         $.ajax({
-             url: url,
-             type: 'POST',
-             data: data,
-             cache: true,
-             success: function (data) {}
-         })
-
-    }
-
-    form.on('submit', function(e){
-        e.preventDefault();
-        var submit_btn = $('#submit_btn');
-        var name = submit_btn.data("name");
-        console.log(name);
-        var nmb = $('#number').val();
-        console.log(nmb);
-
-        basketUpdating(name, nmb)
-
-    });
-
+function initJournal() {
+    $('.day-box').click(function(event){
+        var box = $(this);
+    $.ajax(box.data('url'), {
+      'type': 'POST',
+      'async': true,
+      'dataType': 'json',
+      'data': {
+        'name': box.data('name'),
+        'csrfmiddlewaretoken': $('input[name="csrfmiddlewaretoken"]').val()
+      },
     
-     $(document).on('click', '.delete-item', function(e){
-         e.preventDefault();
-         product_id = $(this).data("product_id")
-         nmb = 0;
-         basketUpdating(product_id, nmb, is_delete=true)
-     })
+    });
+    });
+}
 
+$(document).ready(function(){
+    initJournal();
 });
--- ajax/views.py
+++ ajax/views.py
@@ -1,23 +1,21 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.core.urlresolvers import reverse
 from .models import *
 
 
 def test(request):
     test = Test.objects.all()
-    session_key = request.session.session_key
-    print (session_key)    
-
+    session_key = request.session.session_key 
     return render(request, 'test.html', locals())
 
 
 
 def adding(request):
     return_dict = dict()
-    data = request.POST
-    test_name = data.get("name")
     session_key = request.session.session_key
     fl = request.POST.get('name', '').strip()
+    print (fl)
     data = {}
     data['name'] = fl
     ts = testinCart(**data)
